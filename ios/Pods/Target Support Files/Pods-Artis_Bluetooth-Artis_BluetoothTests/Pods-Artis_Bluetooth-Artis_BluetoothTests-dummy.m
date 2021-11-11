94b35bebffe13505f7599c860ed5280789b1035a
James R. Maxfield <Vercjames@gmail.com>
2021-11-11 11:52:15 -0600

    CRA

---
+++ ios/Pods/Target Support Files/Pods-Artis_Bluetooth-Artis_BluetoothTests/Pods-Artis_Bluetooth-Artis_BluetoothTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Artis_Bluetooth_Artis_BluetoothTests : NSObject
+@end
+@implementation PodsDummy_Pods_Artis_Bluetooth_Artis_BluetoothTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-Artis_Bluetooth/Pods-Artis_Bluetooth-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Artis_Bluetooth : NSObject
+@end
+@implementation PodsDummy_Pods_Artis_Bluetooth
+@end
