7ecb2376bd6ee4f255479d1f8cba2a8a68b13178
James R. Maxfield <Vercjames@gmail.com>
2021-11-11 16:01:36 -0600

    Native Accessory v1

---
+++ ios/BLUEAccessories.m
@@ -0,0 +1,8 @@
+//
+//  BLUEAccessories.m
+//  Artis_Bluetooth
+//
+//  Created by James Robert Maxfield on 11/11/21.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/BLUEAccessories.swift
@@ -0,0 +1,8 @@
+//
+//  BLUEAccessories.swift
+//  Artis_Bluetooth
+//
+//  Created by James Robert Maxfield on 11/11/21.
+//
+
+import Foundation
