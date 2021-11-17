8446c2da41de5ea4d66e8ae8609b1476229708c7
James R. Maxfield <Vercjames@gmail.com>
2021-11-17 14:06:01 -0600

    Ignores Removed

--- ios/Pods/Target Support Files/Pods-Artis_Bluetooth-Artis_BluetoothTests/Pods-Artis_Bluetooth-Artis_BluetoothTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Artis_Bluetooth_Artis_BluetoothTests : NSObject
-@end
-@implementation PodsDummy_Pods_Artis_Bluetooth_Artis_BluetoothTests
-@end
